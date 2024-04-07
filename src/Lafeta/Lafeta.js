e3ab8435fa3f3da99d492b5abfce27da8a50c3c8
Andriu <m.andrey.valeryevich@gmail.com>
2024-04-07 22:52:14 +0300

    added 3 new info sections

---
+++ src/Lafeta/Lafeta.js
@@ -0,0 +1,38 @@
+import Card from 'react-bootstrap/Card';
+import './Lafeta.css'; // Импортируем стили
+
+// Стили для галереи и картинок
+const lafetaStyles = {
+    container: {
+      display: 'flex',
+      justifyContent: 'left',
+      gap: '20px', // Промежуток между картинками
+      padding: '15px', // Отступы для контейнера, чтобы картинки не касались краев экрана
+      paddingTop: '10px', // Добавляем прокрутку, если картинки не помещаются
+      overflow: 'hidden', /* Скрываем всё, что выходит за пределы блока */
+     
+    }
+  };
+
+function Lafeta() {
+  return (
+    <div style={lafetaStyles.container}>
+    <Card style={{ width: '30rem' }} border="info" className="info-block">
+      <Card.Body>
+        <div className="lafeta-line lafeta-first" >Додаткова лафета в наявності</div>
+        <div style={{textAlign:'right'}} className="lafeta-line lafeta-second">Оренда лафети</div>
+        <div style={{textAlign:'right'}} className="lafeta-line lafeta-third mb-2 ">Вантажні перевезення</div>
+       
+        {/* <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link> */}
+      </Card.Body>
+    </Card>
+    </div>
+  );
+}
+
+export default Lafeta;
---
+++ src/Order/Order.js
@@ -0,0 +1,35 @@
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const orderStyles = {
+    container: {
+      display: 'flex',
+      justifyContent: 'center',
+      padding: '15px', // Отступы для контейнера, чтобы картинки не касались краев экрана
+      paddingTop: '10px', // Добавляем прокрутку, если картинки не помещаются
+      overflow: 'hidden', /* Скрываем всё, что выходит за пределы блока */
+      borderRadius: '15px', /* Скругленные углы для контейнера галереи */
+    }
+  };
+  
+function Order() {
+  return (
+    
+    <div style={orderStyles.container}>
+    <Card style={{ width: '25rem' }}>
+      <Card.Header>Як замовити евакуатор !?</Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup.Item>1. Зателефонувати за номером +380508518834</ListGroup.Item>
+        <ListGroup.Item>2. Вказати точну адресу куди потрібен евакуатор</ListGroup.Item>
+        <ListGroup.Item>3. Назвати марку авто ( до 2,5 тонн )</ListGroup.Item>
+        <ListGroup.Item>4. Вказати чи є складність завантаження</ListGroup.Item>
+        <ListGroup.Item>5. Назвати адресу куди потрібно доставити</ListGroup.Item>
+        <ListGroup.Item>6. Назвати марку авто ( до 2,5 тонн )</ListGroup.Item>
+        <ListGroup.Item>7. Очікувати на евакуатор</ListGroup.Item>
+      </ListGroup>
+    </Card>
+    </div>
+  );
+}
+
+export default Order;
